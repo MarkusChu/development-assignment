32b327b5e3d218284459d63de362c2c65293b4e0
MarkusChu <75825637+MarkusChu@users.noreply.github.com>
2022-11-29 10:59:35 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,13 +1,31 @@
 import "../App.css";
 
-// TODO: create a component that displays a single bakery item
 function BakeryItem(props) {
+    const ingredientString = props.ingredients.map((ingredient, index) => {
+        if (index === props.ingredients.length - 1) {
+            return ingredient;
+        } else {
+            return ingredient + ", ";
+        }
+    });
+
+    const priceCategory = (price) => {
+        if (price < 3) {
+            return "Under $3";
+        } else if (price >= 3 && price <= 5) {
+            return "$3 - $5";
+        } else {
+            return "Over $5";
+        }
+    }
+    
     return (
         <div className="BakeryItem">
             <img src={props.image} alt={props.name} width="200" height="200" />
             <p>{props.name}</p>
-            <p>{props.description}</p>
             <p>${props.price}</p>
+            <p>Ingredients: {ingredientString}</p>
+            <p>Price Category: {priceCategory(props.price)}</p>
             <button onClick={() => props.callback(props)}>Add to Cart</button>
         </div>
     );
